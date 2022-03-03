a165a8d45c87f0518e294e52d2a8fc08354906a6
Hongyi Liu <lhysemail@gmail.com>
2022-03-03 19:14:42 +0800

    add retry

--- bachCloneMavenRepo.py
+++ bachCloneMavenRepo.py
@@ -3,8 +3,18 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 
 from git import Repo  # gitpython
-from github import Github
+from github import Github # pygithub
+import logging
 
+logger = logging.getLogger('logger')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh = logging.FileHandler(filename=f'log.txt')
+fh.setFormatter(formatter)
+sh = logging.StreamHandler()
+sh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(sh)
 
 argparser = argparse.ArgumentParser("批量下载Git Repos")
 
@@ -23,35 +33,45 @@ with open(args.repos_file) as f:
 
 # using access_token create github case
 g = Github("ghp_1fPXYOqTXEQNX27U9zv9QmyKB5nv1Pp2zYV6V")
+executor = ThreadPoolExecutor(max_workers=2)
+
+start_epoch = 0
 
 def bulk_clone_repos():
-
+    global start_epoch
     def get_dest(git_repo_url: str):
         return os.path.join(args.project_path, git_repo_url.rsplit(".", maxsplit=1)[0].rsplit("/", maxsplit=1)[1])
 
-    executor = ThreadPoolExecutor(max_workers=10)
-    repos_num = 0
-    epoch_num = 0
-    for repo in repos:
-        epoch_num += 1
-        repo_name = repo.split('.')[-2].split('/')[-1]
-        repo_user = repo.split('.')[-2].split('/')[-2]
-        gitrepo = g.get_repo(repo_user+"/"+repo_name)
-        contents = gitrepo.get_contents("")
-        isMaven = 0
-        for content_file in contents:
-            # print(content_file)
-            if content_file.path == "pom.xml":
-                isMaven = 1
-                break
-        if isMaven == 0:
-            continue
-        repos_num +=1
-        print("epoch_num: {} repos_num: {} Git clone: [{}]".format(epoch_num, repos_num, repo))
-        if args.multi_thread:
-            executor.submit(Repo.clone_from, repo, get_dest(repo))
-        else:
-            Repo.clone_from(url=repo, to_path=get_dest(repo))
+    try:
+        repos_num = 0
+        epoch_num = 0
+        for repo in repos:
+            epoch_num += 1
+            if epoch_num < start_epoch:
+                continue
+            start_epoch = epoch_num
+            repo_name = repo.split('.')[-2].split('/')[-1]
+            repo_user = repo.split('.')[-2].split('/')[-2]
+            gitrepo = g.get_repo(repo_user + "/" + repo_name)
+            contents = gitrepo.get_contents("")
+            isMaven = 0
+            for content_file in contents:
+                # print(content_file)
+                if content_file.path == "pom.xml":
+                    isMaven = 1
+                    break
+            if isMaven == 0:
+                continue
+            repos_num += 1
+            logger.info("epoch_num: {} repos_num: {} Git clone: [{}]".format(epoch_num, repos_num, repo))
+            if args.multi_thread:
+                executor.submit(Repo.clone_from, repo, get_dest(repo))
+            else:
+                Repo.clone_from(url=repo, to_path=get_dest(repo))
+    except:
+        bulk_clone_repos()
+
+
 
 if __name__ == "__main__":
     bulk_clone_repos()
